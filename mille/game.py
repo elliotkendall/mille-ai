7a699ea5693ed34ec451ec4e0b8e8cf96f2de49c
Matthew Sachs <matthewg@zevils.com>
2014-12-29 07:59:48 -0800

    Move cardDrawn notification logic from Deck to Game

--- mille/game.py
+++ mille/game.py
@@ -105,7 +105,7 @@ class Game:
 
     # Deal hands
     for player in self.players:
-      player.hand = self.deck.draw(player, 6)
+      player.hand = self.draw(player, 6)
 
     # Reset per-hand team variables
     for team in self.teams:
@@ -121,7 +121,7 @@ class Game:
       currentTeam = self.teams[currentPlayer.teamNumber]
 
       try:
-        currentPlayer.hand.append(self.deck.draw(currentPlayer))
+        currentPlayer.hand.append(self.draw(currentPlayer))
       except IndexError:
         self.delayedAction = True
 
@@ -231,7 +231,7 @@ class Game:
             # Draw an extra card to replace the one just played
             try:
               player = self.players[coupFourrePlayerNumber]
-              player.hand.append(self.deck.draw(player))
+              player.hand.append(self.draw(player))
             except IndexError:
               pass
             targetTeam.coupFourres += 1
@@ -350,3 +350,12 @@ class Game:
     state.target = self.target
     state.cardsLeft = self.deck.cardsLeft()
     return state
+
+  def draw(self, player, count = 1):
+    cards = self.deck.draw(count)
+    if count == 1:
+      player.ai.cardDrawn(cards)
+    else:
+      for card in cards:
+        player.ai.cardDrawn(card)
+    return cards
